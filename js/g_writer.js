79baeb049a5bf225cad15da2a67550cded6b7c33
Nakashima723 <yokoshima723@gmail.com>
2023-05-31 17:57:21 +0900

    報告機能を、クリップボードを利用する形で改修

--- js/g_writer.js
+++ js/g_writer.js
@@ -12,115 +12,108 @@
   }
   //未報告URLが存在する場合
   else {
-    var footerStart = function (selector) {
-      $('html,body').animate({
-        scrollTop: $(selector).offset().top
-      }, 'fast');
-    }
-    $("body").append("<span id='item'></span>");
     $(function () {
-      setTimeout(function () {
-        var date = new Date();
-        var month = date.getMonth() + 1,
-          day = date.getDate();
-        if (month < 10) {
-          month = "0" + month;
-        }
-        if (day < 10) {
-          day = "0" + day;
-        }
-        date = month + "/" + day + "/" + date.getFullYear();
-        var fullname = items.m_family + " " + items.m_name;
-        for (var i = 0; i < thisArr.length; i++) {
-          var url = $('[name=infringing-urls0]').val();
-          url = url + thisArr[i] + "\n";
-          $('[name=infringing-urls0]').val(url);
-        }
-        var infringement = items['m_infringement' + mode];
-        var original = items['m_original' + mode];
-        var holder = items['m_holder' + mode];
-        $(function () {
-          $('material-checkbox').each(function (index) {
-            setTimeout(function () {
-              $(this).attr('aria-checked', true);
-            }.bind(this), index * 1000);
-          });
-          $('.form-title').prepend("<button id=\"test\">test</button>");
-          $('#test').on('click', function () {
-            $('input[type="text"]').first().val("test");
-            document.execCommand("paste");
-          });
-          $('[name=first-name]').val(items.m_name);
-          $('[name=last-name]').val(items.m_family);
-          $('[name=company-name]').val(items.m_company);
-          $('[name=email]').val(items.m_email);
-          $('[name=country-code]').val("JP");
-          $('[name=cr-work-desc0]').val(original);
-          $('[name=cr-work-urls0]').val(infringement);
-          if (holder === "self" || holder === undefined) {
-            $('[name=confirm-self-cr-holder]').prop('checked', true);
-            $('[name=cr-holder]').val("self");
-          } else {
-            $('[name=cr-holder]').val(holder);
-          }
-          $('[name=agree1]').prop('checked', true);
-          $('[name=agree2]').prop('checked', true);
-          $('[name=agree5]').prop('checked', true);
-          $('[name=signature-date]').val(date);
-          $('[name=signature]').val(fullname);
-          $('#add-group-link').click(function () {
-            var href = jQuery(this).attr("href");
-            location.href = href;
-          });
-          var mode = items.m_mode
-          var thisArr = items['m_Arr' + mode];
-          var thisSiteArr = items['m_SiteArr' + mode];
-          var thisDateArr = items['m_DateArr' + mode];
-          var thisFinArr = items['m_FinArr' + mode];
-          var thisFinSiteArr = items['m_FinSiteArr' + mode];
-          var thisFinDateArr = items['m_FinDateArr' + mode];
-          var thisTitle = items['m_report' + mode];
-          var num = mode + 1;
-          var urlNum = thisArr.length;
-          if (urlNum > 1000) {
-            urlNum = 1000;
-          }
+        alert("「" + num + ": " + thisTitle + "」について、" + urlNum + "件のURLを入力します。\n\n空のテキスト入力欄をクリックすると、それぞれの欄に対応したデータが【クリップボードに入力】されます。そのままペースト（Ctrl+Vなど）の操作を行って、順に空欄を埋めてください。\n\n・今あるクリップボードの内容を消したくない場合は、入力を中止してください。\n・ラジオボタンや宣誓のチェックボックス、reCAPCHAは手動で入力する必要があることに注意してください。");
+
+      var date = new Date();
+      var month = date.getMonth() + 1,
+        day = date.getDate();
+      if (month < 10) {
+        month = "0" + month;
+      }
+      if (day < 10) {
+        day = "0" + day;
+      }
+      date = month + "/" + day + "/" + date.getFullYear();
+      var fullname = items.m_family + " " + items.m_name;
+      var url = ""
+      for (var i = 0; i < thisArr.length; i++) {
+        url = url + thisArr[i] + "\n";
+      }
+      var infringement = items['m_infringement' + mode];
+      var original = items['m_original' + mode];
+      var holder = items['m_holder' + mode];
+		
+		$(document).ready(function() {
+    $('button').prop('disabled', false);
+	$(document).on('click', 'button', function() {
+		var index = $('button').index(this) + 1;
+		if(index === 5) {
+			var mode = items.m_mode
+			var thisArr = items['m_Arr' + mode];
+			var thisSiteArr = items['m_SiteArr' + mode];
+			var thisDateArr = items['m_DateArr' + mode];
+			var thisFinArr = items['m_FinArr' + mode];
+			var thisFinSiteArr = items['m_FinSiteArr' + mode];
+			var thisFinDateArr = items['m_FinDateArr' + mode];
+			var thisTitle = items['m_report' + mode];
+			var num = mode + 1;
+			var urlNum = thisArr.length;
+			if (urlNum > 1000) {
+			  urlNum = 1000;
+			}
+			for (i = 0; i < urlNum; i++) {
+			  thisFinArr.push(thisArr[i]);
+			  thisFinSiteArr.push(thisSiteArr[i]);
+			}
+			thisArr.splice(0, urlNum);
+			thisSiteArr.splice(0, urlNum);			
+          var obj = {};
+          var date = new Date();
+          date = date.toLocaleString();
           for (i = 0; i < urlNum; i++) {
-            thisFinArr.push(thisArr[i]);
-            thisFinSiteArr.push(thisSiteArr[i]);
+            thisFinDateArr.push(date);
           }
-          thisArr.splice(0, urlNum);
-          thisSiteArr.splice(0, urlNum);
-          $('input[type="submit"]').click(function () {
-            var obj = {};
-            var date = new Date();
-            date = date.toLocaleString();
-            for (i = 0; i < urlNum; i++) {
-              thisFinDateArr.push(date);
-            }
-            thisDateArr.splice(0, urlNum);
-            obj['m_Arr' + mode] = thisArr;
-            obj['m_DateArr' + mode] = thisDateArr;
-            obj['m_SiteArr' + mode] = thisSiteArr;
-            obj['m_FinArr' + mode] = thisFinArr;
-            obj['m_FinDateArr' + mode] = thisFinDateArr;
-            obj['m_FinSiteArr' + mode] = thisFinSiteArr;
-            chrome.storage.local.set(obj, function (items) {});
-            alert("「" + num + ": " + thisTitle + "」について、" + urlNum + "件のURLを送信し、報告ずみURLとして記録しました。");
+          thisDateArr.splice(0, urlNum);
+          obj['m_Arr' + mode] = thisArr;
+          obj['m_DateArr' + mode] = thisDateArr;
+          obj['m_SiteArr' + mode] = thisSiteArr;
+          obj['m_FinArr' + mode] = thisFinArr;
+          obj['m_FinDateArr' + mode] = thisFinDateArr;
+          obj['m_FinSiteArr' + mode] = thisFinSiteArr;
+          chrome.storage.local.set(obj, function (items) {});
+          alert("「" + num + ": " + thisTitle + "」について、" + urlNum + "件のURLを送信し、報告ずみURLとして記録しました。");
+		}
+	});
+    $('button').prop('disabled', true);
+		});
+
+      function copyToClipboard(textToCopy) {
+        navigator.clipboard.writeText(textToCopy)
+          .then(() => {
+            console.log('「' + textToCopy + '」をクリップボードにコピーしました');
+          })
+          .catch(err => {
+            console.error('Could not copy text: ', err);
           });
-          alert("「" + num + ": " + thisTitle + "」について、" + urlNum + "件のURLを入力します。\n\n記入内容を確認し、ページ末尾のチェックボックス（reCAPCHA）にチェックを入れてから、送信ボタンを押してください。");
-          // $('#add-group-link').trigger("click");
+      }
+      $(function () {
+        $('input').eq(1).on('click', function () {
+          copyToClipboard(items.m_name);
         });
-        if (items.m_author == "attoney") {
-          setTimeout(function () {
-            footerStart("#dmca");
-          }, 500);
-        } else {
-          setTimeout(function () {
-            footerStart("#item");
-          }, 500);
-        }
-      }, 200);
+        $('input').eq(2).on('click', function () {
+          copyToClipboard(items.m_family)
+        });
+        $('input').eq(3).on('click', function () {
+          copyToClipboard(items.m_company)
+        });
+        $('input').eq(4).on('click', function () {
+          copyToClipboard(items.email)
+        });
+        $('input').eq(5).on('click', function () {
+          copyToClipboard(fullname)
+        });
+        $('textarea').eq(0).on('click', function () {
+          copyToClipboard(original);
+        });
+        $('textarea').eq(1).on('click', function () {
+          copyToClipboard(infringement);
+        });
+        $('textarea').eq(2).on('click', function () {
+          copyToClipboard(url);
+        });
+        // $('#add-group-link').trigger("click");
+      });
     }); //setTimeoutの終わり
   } //未報告URLが存在する場合の終わり
 });
